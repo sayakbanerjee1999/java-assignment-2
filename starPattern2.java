68fefcd6dcde391eb9daefd7166e59d9d8cb6345
Sayak Banerjee <43917478+sayakbanerjee1999@users.noreply.github.com>
2022-01-21 15:18:43 +0530

    Added Star Pattern 2

---
+++ starPattern2.java
@@ -0,0 +1,16 @@
+public class assignment2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Scanner in = new Scanner(System.in);
+		
+		int n = 5;
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n-i; j++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+	}
+
+}
